1786583f015949ec9ab201bcb25b8bf39fd07688
bbooss97 <bbooss97@gmail.com>
2021-04-12 00:41:32 +0200

    risolvo alcuni problemi che si erano creati con la rimozione

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,12 @@ class HomeController < ApplicationController
   def admin
     if params[:id]=="admin12345"
       daRimuovere=params[:daRimuovere]
-      Visite.find_by(id: daRimuovere).try(:destroy)
+      puts daRimuovere
+      Visite.where(email: daRimuovere).try(:delete_all)
+      User.where(email: daRimuovere).try(:delete_all)
+      Paziente.where(email: daRimuovere).try(:delete_all)
+       
+      
       render html:"ho eliminato se trovato"
     else
       render html:"password sbagliata"
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'home/index' , to:'home#index'
   root to: 'home#index'
   get 'home/about'
-  get 'admin/:id/:daRimuovere' ,to:"home#admin"
+  get 'admin/:id/:daRimuovere' ,to:"home#admin", :constraints => { daRimuovere: /[^\/]+/ }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
